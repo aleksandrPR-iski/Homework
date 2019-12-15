4902affcd9b128c7e29ba10891ea30f2f5c31c2d
aleksandrPR-iski <Presnyakov.sasha01@mail.ru>
2019-12-15 23:58:38 +0500

    Условные операторы
    .

---
+++ Условные операторы 2/Условные операторы 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Условные_операторы_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                    Console.WriteLine("Введите символ");
+                char x = char.Parse(Console.ReadLine());
+                if (char.IsDigit(x))
+                    Console.WriteLine("Это цифра");
+                if (char.IsLower(x))
+                    Console.WriteLine("Это строчная буква");
+                if (char.IsUpper(x))
+                    Console.WriteLine("Это заглавная буква");
+                if (char.IsPunctuation(x))
+                    Console.WriteLine("Это знак пунктуации");
+                if (char.IsWhiteSpace(x))
+                    Console.WriteLine("Это пробел");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Условные операторы 3/Условные операторы 3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Условные операторы 3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Условные операторы 3")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a4afb379-0412-41e7-a700-f2858df267ea")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
